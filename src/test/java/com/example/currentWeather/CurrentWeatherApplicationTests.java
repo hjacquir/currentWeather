5585da66621dc9650d00deadd49681e88143ed5f
hjacquir <jacquirhatim@gmail.com>
2025-03-20 08:59:59 +0100

    initial commit

---
+++ src/test/java/com/example/currentWeather/CurrentWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currentWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrentWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
